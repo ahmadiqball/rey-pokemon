9f3ab8a0635ba3741be17d44d0d440dfa68c9817
ahmadiqbal-incentro <ahmad.iqbal@incentro.com>
2024-11-17 18:54:02 +0800

    chore: set up material ui theme config

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,7 @@
 import type { Metadata } from 'next';
-import localFont from 'next/font/local';
 import './globals.css';
-
-const geistSans = localFont({
-  src: './fonts/GeistVF.woff',
-  variable: '--font-geist-sans',
-  weight: '100 900',
-});
-const geistMono = localFont({
-  src: './fonts/GeistMonoVF.woff',
-  variable: '--font-geist-mono',
-  weight: '100 900',
-});
+import { ThemeProvider } from '@emotion/react';
+import { theme } from '~/designs/theme.config';
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -24,10 +14,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
-      </body>
-    </html>
+    <ThemeProvider theme={theme}>
+      <html lang="en">
+        <body>{children}</body>
+      </html>
+    </ThemeProvider>
   );
 }
---
+++ src/designs/theme.config.ts
@@ -0,0 +1,50 @@
+import { Color, createTheme } from '@mui/material';
+
+declare module '@mui/material/styles/createPalette' {
+  interface Palette {
+    yellow: Partial<Color>;
+    blue: Partial<Color>;
+    green: Partial<Color>;
+    orange: Partial<Color>;
+    red: Partial<Color>;
+  }
+  // allow configuration using `createTheme()`
+  interface PaletteOptions {
+    yellow?: Partial<Color>;
+    blue?: Partial<Color>;
+    green?: Partial<Color>;
+    orange?: Partial<Color>;
+    red?: Partial<Color>;
+  }
+}
+
+export const theme = createTheme({
+  palette: {
+    common: {
+      black: '#42494D',
+    },
+    grey: {
+      '400': '#F7F8F8',
+      '600': '#B3B6B8',
+      '700': '#7B8082',
+    },
+    yellow: {
+      '200': '#FFD86C',
+      '300': '#FFCB3B',
+      '400': '#E6AB09',
+    },
+    blue: {
+      '200': '#0571A6',
+      '400': '#3C48CF',
+    },
+    orange: {
+      '400': '#E66D00',
+    },
+    green: {
+      '400': '#01B956',
+    },
+    red: {
+      '400': '#DE2C2C',
+    },
+  },
+});
